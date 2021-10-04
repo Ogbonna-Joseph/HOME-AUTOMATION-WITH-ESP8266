e116227fa0925c487efe8215daeda43da6bd2c47
Joseph Ogbonna <58666630+Ogbonna-Joseph@users.noreply.github.com>
2021-10-04 14:30:26 +1200

    Update AUTOMATION.cpp

--- AUTOMATION.cpp
+++ AUTOMATION.cpp
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
  
 // Replace with your network credentials
-const char* ssid     = "MySmartHome";   //wifi name
-const char* password = "hngzhowxiantan";      //wifi password
+const char* ssid     = "********";   //wifi name
+const char* password = "********";      //wifi password
  
 // Set web server port number to 80
 WiFiServer server(80);
